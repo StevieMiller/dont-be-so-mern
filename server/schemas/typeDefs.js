5ef78d6b49b95be43e5e19c258a2d2c451c33c8d
StevieMiller <stephanie.miller@carlsbadschools.net>
2023-04-26 09:09:10 -0600

    added typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,47 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    bookCount: Int
+    savedBooks: [Book]!
+  }
+
+  type Book {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    users: [User]
+    user(username: String!): User
+    books(username: String): [Book]
+    book(bookId: String!): Book
+    me: User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(bookId: String!): User
+    deleteBook(bookId: String!): User
+
+    addComment(thoughtId: ID!, commentText: String!): Thought
+    removeThought(thoughtId: ID!): Thought
+    removeComment(thoughtId: ID!, commentId: ID!): Thought
+  }
+`;
+
+module.exports = typeDefs;
