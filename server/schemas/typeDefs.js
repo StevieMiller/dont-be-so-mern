1a2469bb95a983a0c2a53b9cd4e74926f7a0e795
StevieMiller <stephanie.miller@carlsbadschools.net>
2023-04-28 15:00:56 -0600

    fixing mutations and typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,15 +1,22 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+  type Query {
+    me: User
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(book: inputBook!): User
+    removeBook(bookId: String!): User
+  }
   type User {
     _id: ID
     username: String
     email: String
-    password: String
     bookCount: Int
     savedBooks: [Book]
   }
-
   type Book {
     bookId: String
     authors: [String]
@@ -18,25 +25,9 @@ const typeDefs = gql`
     image: String
     link: String
   }
-
   type Auth {
     token: ID!
     user: User
-  }
-
-  type Query {
-    users: [User]
-    user(username: String!): User
-    books(username: String): [Book]
-    book(bookId: String!): Book
-    me: User
-  }
-
-  type Mutation {
-    addUser(username: String!, email: String!, password: String!): Auth
-    login(email: String!, password: String!): Auth
-    saveBook(bookId: String!): User
-    deleteBook(bookId: String!): User
   }
 `;
 
